30ab7a8c4cdc453c61509b9c71d5c6670ecd1037
IbrahimYolb1r <85681843+IbrahimYolb1r@users.noreply.github.com>
2022-02-27 23:10:05 +1100

    init commit

---
+++ src/keys.js
@@ -0,0 +1,3 @@
+export const API_KEY = "AIzaSyATeZLL9-m6WV0RdGcuL_HNxAWp338RG2Y"
+
+export default API_KEY
---
+++ src/useGoogleSearch.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import API_KEY from "./keys";
+
+const CONTEXT_KEY = "ee15402aa521f41e0";
+
+const useGoogleSearch = (term) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          setData(result);
+        });
+    };
+    fetchData();
+  }, [term]);
+  return { data };
+};
+
+export default useGoogleSearch;
